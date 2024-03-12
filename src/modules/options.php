53c8b355d56f75cda9e40a9da190fe25d3d96297
Javier Troya <javier.troya@mediamechanics.com>
2024-03-12 12:18:24 +0100

    GTM. Remove commented code.

--- src/modules/options.php
+++ src/modules/options.php
@@ -2,18 +2,6 @@
 
 namespace mody\options;
 
-//add_action( 'acf/init', function () {
-//	$site_url = MODY_PLUGIN_DIR_URL;
-//	acf_add_options_page( array(
-//		'page_title' => __( 'Mody Options', 'mody' ),
-//		'menu_slug'  => 'mody-options',
-//		'icon_url'   => "{$site_url}assets/img/mody-solutions-iso-white.png",
-//		'menu_title' => 'Mody',
-//		'position'   => 100,
-//		'redirect'   => false,
-//	) );
-//} );
-
 add_action( 'admin_menu', function () {
 	$user    = wp_get_current_user();
 	$roles   = [ 'administrator' ];
